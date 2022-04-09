3f7b0a5222d04a05322114e0c091f7a8c1916de8
Paramon, Yevstigneyev <90145033+ParamonPlay2205@users.noreply.github.com>
2022-04-09 09:48:24 -0700

    Update clear_dns.ps1

--- clear_dns.ps1
+++ clear_dns.ps1
@@ -3,8 +3,16 @@ Write-Host "PowerShell Game Enhancer`n"
 # Infinte While-Loop that clears the user's DNS until they press Ctrl + c.
 while($true)
 {
+    # Randomly generates a number (in seconds) for the DNS to be cleared again
+    $delay = Get-Random 30,60,90,120,150,180;
     # Runs the ipconfig flush dns command to flush the user's DNS
     ipconfig /flushdns;
-    # Adds a 2 minute delay to make the script flush the DNS every 2 minutes
-    sleep 120
+    # Clears the console window after the user's DNS is cleared to keep it clean.
+    Clear-Host;
+    
+    # Shows the user that their DNS has been flushed.
+    Write-Host "`nPowerShell Game Enhancer`nCleared DNS...";
+    
+    # Adds a random delay for the DNS to be cleared
+    sleep $delay
 }
